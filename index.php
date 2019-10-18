4684bc2517ca32f727d95d1ab5a7703b7474a926
Kellis <34947108+Gumbraise@users.noreply.github.com>
2019-10-19 01:36:16 +0200

    v0.1910

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>LeBonF</title>
+</head>
+<header>
+</header>
+<body>
+<div class="posts">
+    <div class="post">
+        <div class="top">
+            <div class="pp">
+                <img src="https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/72696640_404517410448630_6411701761299447808_n.jpg?_nc_cat=104&_nc_oc=AQkLP2bt5tK2aaPKSOT57vxeQR36Jg8D5CrUUjMmAy9V5zrJeU8DD0l4J7-48z1bzgM&_nc_ht=scontent-cdg2-1.xx&oh=429ebfb0e4457851512b995042ab66b0&oe=5E30065C">
+            </div>
+            <div class="tet">
+                <a href="#"><p class="name">Lorem Ipsum</p></a>
+                <p class="salle">F202</p>
+            </div>
+            <div class="price">
+                <p class="price">20€</p>
+            </div>
+        </div>
+        <div class="body">
+            <p class="tett">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias vel beatae at et, officia dolorem molestiae hic distinctio iure? Perspiciatis tempore fugit voluptatum consequuntur illo optio blanditiis magni ipsam aliquam.</p>
+        </div>
+    </div>
+    <div class="fil">
+
+    </div>
+</div>
+<div class="pub">
+
+</div>
+</body>
+<footer>
+</footer>
+<script src="assets/js/main.js"></script>
+</html>
