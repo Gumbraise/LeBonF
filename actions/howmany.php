30ac226d981c5ca1773cf13ade4574c0df46fe8a
Kellis <34947108+Gumbraise@users.noreply.github.com>
2019-10-21 16:45:19 +0200

    Delete howmany.php

--- actions/howmany.php
+++
@@ -1,9 +0,0 @@
-<?php
-$bdd = new PDO('mysql:host=localhost;dbname=lebonf', 'root', '');
-
-$itemlist = $bdd->query("SELECT COUNT(*) as id FROM vente"); 
-$itemlists = $itemlist->fetch();
-$itemlist->closeCursor();
-
-echo $itemlists["id"];
-?>
